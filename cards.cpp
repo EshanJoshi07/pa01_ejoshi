be54771e1f561a8f58db3bd30de540287347ff6e
Eshan Joshi <eshanjoshi@cs.ucsb.edu>
2023-03-06 22:24:50 -0800

    test

--- cards.cpp
+++ cards.cpp
@@ -1,5 +1,5 @@
 // cards.cpp
-//NAME: ESHAN JOSHI
+// NAME: ESHAN JOSHI
 // Implementation of the classes defined in cards.h
 
 #include <iostream>
@@ -10,432 +10,109 @@
 
 using namespace std;
 
-// //constructor --> set the root as null
-// Cards::Cards() {
-//     root = nullptr;
-// }
-
-// // destructor deletes all nodes
-// Cards::~Cards() {
-//     clear(root);
-// }
-
-// //destructor helper --> recrusive 
-// void Cards::clear(Node *n) {
-//     if (n) {
-// 	    clear(n->left);
-// 	    clear(n->right);
-// 	    delete n;
-//     }
-// }
-
-// //insert value in tree; return false if duplicate
-// bool Cards::insert(int value) {
-//     Node *temp = root;
-
-//     //check if tree is empty
-//     if (!temp) {
-// 	    temp = new Node(value);
-// 	    return true;
-//     }
-
-//     return insert(value, temp);
-// }
-
-// //insert helper (assumes n is never 0) --> recrusive 
-// bool Cards::insert(int value, Node *n) {
-//     //check if value = n->info
-//     if (value == n->info)
-// 	    return false;
-
-//     //check if value <n->info --> if value is less also check the left node
-//     if (value < n->info) {
-// 	    if (n->left) {
-// 	        return insert(value, n->left);
-//         }
-// 	    else {
-// 	        n->left = new Node(value);
-// 	        n->left->parent = n;
-// 	        return true;
-// 	    }
-//     }
-
-//     //check if value>n->info --> if value is greater also check the left node
-//     else {
-// 	    if (n->right) {
-// 	        return insert(value, n->right);
-//         }
-// 	    else {
-// 	        n->right = new Node(value);
-// 	        n->right->parent = n;
-// 	        return true;
-// 	    }
-//     }
-// }
-
-// //printPreOrder
-// void Cards::printPreOrder() const {
-//     Node *n = root;
-//     printPreOrder(n);
-// }
-
-// //printPreOrder helper --> recursive 
-// void Cards::printPreOrder(Node *n) const {
-//     if (n) {
-// 	    cout << n->info << " ";
-// 	    printPreOrder(n->left);
-// 	    printPreOrder(n->right);
-//     }
-// }
-
-// //printInOrder
-// void Cards::printInOrder() const {
-//     Node *n = root;
-//     printInOrder(n);
-// }
-
-// //printInOrder helper --> recursive
-// void Cards::printInOrder(Node *n) const {
-//     if (n == NULL){
-//         return;
-//     }
-//     printInOrder(n->left);
-//     cout << n->info << " ";
-//     printInOrder(n->right);
-// }
-
-// //printPostOrder
-// void Cards::printPostOrder() const {
-//     Node *n = root;
-//     printPostOrder(n);
-// }
-
-// //printPostOrder helper --> recursive
-// void Cards::printPostOrder(Node *n) const {
-//     if (n == NULL){
-//         return;
-//     }
-//     printPostOrder(n->left);
-//     printPostOrder(n->right);
-//     cout << n->info << " ";
-//     // IMPLEMENT HERE
-// }
-
-// int Cards::sum() const {
-//     Node *n = root;
-//     return sum(n);
-// }
-
-// //sum helper --> recursive
-// int Cards::sum(Node *n) const {
-//     if (n == NULL){
-//         return 0;
-//     }
-//     return (n->info + sum(n->left) + sum(n->right));
-// }
-
-// //counter
-// int Cards::count() const {
-//     Node *n = root;
-//     return count(n);
-// }
-
-// //counter hlper --> recurvsive
-// int Cards::count(Node *n) const {
-//     if (n == NULL){
-//         return 0;
-//     }
-//     return (1 + count(n->left) + count(n->right));
-// }
-
-// // Parameters:
-// // int value: the value to be found
-// // Node* n: the node to start with (for a recursive call)
-// // Whenever you call this method from somewhere else, pass it
-// // the root node as "n"
-// Cards::Node* Cards::getNodeFor(int value, Node* n) const{
-//     if(n==NULL){
-//         return NULL;
-//     }
-//     else if(n->info==value){
-//         return n;
-//     }
-//     else if(n->left==NULL && n->right==NULL){
-//         return NULL;
-//     }
-//     else{
-//         if(n->info>value){
-//             return getNodeFor(value,n->left);
-//         }
-//         else{
-//             return getNodeFor(value,n->right);
-//         }
-//     }
-//     // REPLACE THIS NON-SOLUTION
-// }
-
-// // returns true if value is in the tree; false if not
-// bool Cards::contains(int value) const {
-    
-//     return getNodeFor(value,root)!= NULL;//IS THIS ALLOWED?
-
-//      // REPLACE THIS NON-SOLUTION
-// }
-
-// // returns the Node containing the predecessor of the given value
-// Cards::Node* Cards::getPredecessorNode(int value) const{
-//     Node* loc = getNodeFor(value,root);
-//     Node* smallest = root;
-//     while(smallest->left){
-//         smallest = smallest->left;
-//     }
-//     if(loc==smallest){
-//         return 0;
-//     }
-//     else{
-//         if(loc->left){
-//             loc=loc->left;
-//             while(loc->right){
-//                 loc=loc->right;
-//             }
-//         }
-//         /*else if(loc->right){
-//             loc=loc->right;
-//             while(loc->left){
-//                 loc=loc->left;
-//             }
-//         }*/
-//         else{
-//             while(loc->parent){
-//                 if(loc->parent->right==loc){
-//                     return loc->parent;
-//                 }
-//                 loc=loc->parent;
-//             }
-//         }
-//         return loc;
-//         //return loc;
-        
-//     }
-    
-// }
-
-// // returns the predecessor value of the given value or 0 if there is none
-// int Cards::getPredecessor(int value) const{
-//     if(getPredecessorNode(value)==NULL){
-//         return 0;
-//     }
-//     else{
-//         return getPredecessorNode(value)->info; //IS THIS ALLOWED?
-//     }
-// }
-
-// // returns the Node containing the successor of the given value
-// Cards::Node* Cards::getSuccessorNode(int value) const{
-    
-//     Node* loc = getNodeFor(value,root);
-//     Node* largest = root;
-//     while(largest->right){
-//         largest = largest->right;
-//     }
-//     if(loc==largest){
-//         return 0;
-//     }
-//     else{
-//         if(loc->right){
-//             loc=loc->right;
-//             while(loc->left){
-//                 loc=loc->left;
-//             }
-//         }
-//         else{
-//             while(loc->parent){
-//                 if(loc->parent->left==loc){
-//                     return loc->parent;
-//                 }
-//                 loc=loc->parent;
-//             }
-//             /*loc=loc->parent;
-//             while(loc->left==NULL){
-//                 loc=loc->parent;
-//             }*/
-            
-//         }
-//         return loc;
-        
-//     }
-//      // REPLACE THIS NON-SOLUTION
-// }
-
-// // returns the successor value of the given value or 0 if there is none
-// int Cards::getSuccessor(int value) const{
-//     if(getSuccessorNode(value)==NULL){
-//         return 0;
-//     }
-//     else{
-//         return getSuccessorNode(value)->info;
-//     }
-//     //IS THIS ALLOWED?
-// }
-
-// // deletes the Node containing the given value from the tree
-// // returns true if the node exist and was deleted or false if the node does not exist
-// bool Cards::remove(int value){
-//     Node* loc = getNodeFor(value,root);
-//     if(loc==NULL){
-//         return false;
-//     }
-//     if(loc->left==NULL && loc->right==NULL){
-//         if(loc==root){
-//             root = NULL;
-//         }
-//         else if(loc->parent->left==loc){
-//             loc->parent->left=NULL;
-//         }
-//         else{
-//             loc->parent->right=NULL;
-//         }
-//         delete loc;
-//         return true;
-//     }
-//     else if(loc->left && loc->right){
-//         int val = getPredecessor(loc->info);//getSuccessor
-//         remove(getPredecessor(loc->info));
-//         loc->info=val;
-
-//         return true; // REPLACE THIS NON-SOLUTION
-//     }
-//     else{
-//         Node* child;
-//         if(loc->left){
-//             child = loc->left;
-//         }
-//         else{
-//             child = loc->right;
-//         }
-//         if(loc!=root){
-//             if(loc->parent->left==loc){
-//                 loc->parent->left=child;
-//             }
-//             else{
-//                 loc->parent->right=child;
-//             }
-//             child->parent=loc->parent;
-//         }
-//         else{
-//             root=child;
-//         }
-//         delete loc;
-//         return true;
-//     }
-
-// }
-
-// //left side has larger values
-// int Cards::minValue() const{
-//     Node *min = root;
-//     while(min->left){
-//         min = min->left;
-//     }
-//     return min->info;
-// }
-
-// //right side has larger values
-// int Cards::maxValue() const {
-//     Node *max = root;
-//     while(max->right){
-//         max = max->right;
-//     }
-//     return max->info;
-// }
-
-Cards::Cards() : root(nullptr) { }
+//constructor --> set the root as null
+Cards::Cards() {
+    root = nullptr;
+}
 
 // destructor deletes all nodes
 Cards::~Cards() {
     clear(root);
 }
 
-// recursive helper for destructor
+//destructor helper --> recrusive 
 void Cards::clear(Node *n) {
     if (n) {
-	clear(n->left);
-	clear(n->right);
-	delete n;
+	    clear(n->left);
+	    clear(n->right);
+	    delete n;
     }
 }
 
-// insert value in tree; return false if duplicate
+//insert value in tree; return false if duplicate
 bool Cards::insert(int value) {
-    // handle special case of empty tree first
-    if (!root) {
-	root = new Node(value);
-	return true;
+    Node *temp = root;
+
+    //check if tree is empty
+    if (!temp) {
+	    temp = new Node(value);
+	    return true;
     }
-    // otherwise use recursive helper
-    return insert(value, root);
+
+    return insert(value, temp);
 }
 
-// recursive helper for insert (assumes n is never 0)
+//insert helper (assumes n is never 0) --> recrusive 
 bool Cards::insert(int value, Node *n) {
+    //check if value = n->info
     if (value == n->info)
-	return false;
+	    return false;
+
+    //check if value <n->info --> if value is less also check the left node
     if (value < n->info) {
-	if (n->left)
-	    return insert(value, n->left);
-	else {
-	    n->left = new Node(value);
-	    n->left->parent = n;
-	    return true;
-	}
+	    if (n->left) {
+	        return insert(value, n->left);
+        }
+	    else {
+	        n->left = new Node(value);
+	        n->left->parent = n;
+	        return true;
+	    }
     }
+
+    //check if value>n->info --> if value is greater also check the left node
     else {
-	if (n->right)
-	    return insert(value, n->right);
-	else {
-	    n->right = new Node(value);
-	    n->right->parent = n;
-	    return true;
-	}
+	    if (n->right) {
+	        return insert(value, n->right);
+        }
+	    else {
+	        n->right = new Node(value);
+	        n->right->parent = n;
+	        return true;
+	    }
     }
 }
 
-// print tree data pre-order
+//printPreOrder
 void Cards::printPreOrder() const {
-    printPreOrder(root);
+    Node *n = root;
+    printPreOrder(n);
 }
 
-// recursive helper for printPreOrder()
+//printPreOrder helper --> recursive 
 void Cards::printPreOrder(Node *n) const {
     if (n) {
-	cout << n->info << " ";
-	printPreOrder(n->left);
-	printPreOrder(n->right);
+	    cout << n->info << " ";
+	    printPreOrder(n->left);
+	    printPreOrder(n->right);
     }
 }
 
-// print tree data in-order, with helper
+//printInOrder
 void Cards::printInOrder() const {
-    printInOrder(root);
+    Node *n = root;
+    printInOrder(n);
 }
+
+//printInOrder helper --> recursive
 void Cards::printInOrder(Node *n) const {
-    if(n==NULL){
+    if (n == NULL){
         return;
     }
     printInOrder(n->left);
     cout << n->info << " ";
     printInOrder(n->right);
-
-    // IMPLEMENT HERE
 }
 
-// prints tree data post-order, with helper
+//printPostOrder
 void Cards::printPostOrder() const {
-    printPostOrder(root);
+    Node *n = root;
+    printPostOrder(n);
 }
 
+//printPostOrder helper --> recursive
 void Cards::printPostOrder(Node *n) const {
-    if(n==NULL){
+    if (n == NULL){
         return;
     }
     printPostOrder(n->left);
@@ -444,36 +121,31 @@ void Cards::printPostOrder(Node *n) const {
     // IMPLEMENT HERE
 }
 
-// return sum of values in tree
 int Cards::sum() const {
-    return sum(root);
+    Node *n = root;
+    return sum(n);
 }
 
-// recursive helper for sum
+//sum helper --> recursive
 int Cards::sum(Node *n) const {
-    if(n==NULL){
+    if (n == NULL){
         return 0;
     }
-    else{
-        return n->info+sum(n->left)+sum(n->right);
-    }
-    // REPLACE THIS NON-SOLUTION
+    return (n->info + sum(n->left) + sum(n->right));
 }
 
-// return count of values
+//counter
 int Cards::count() const {
-    return count(root);
+    Node *n = root;
+    return count(n);
 }
 
-// recursive helper for count
+//counter hlper --> recurvsive
 int Cards::count(Node *n) const {
-    if(n==NULL){
+    if (n == NULL){
         return 0;
     }
-    else{
-        return 1+count(n->left)+count(n->right);
-    }
-    // REPLACE THIS NON-SOLUTION
+    return (1 + count(n->left) + count(n->right));
 }
 
 // Parameters:
@@ -505,7 +177,7 @@ Cards::Node* Cards::getNodeFor(int value, Node* n) const{
 // returns true if value is in the tree; false if not
 bool Cards::contains(int value) const {
     
-    return getNodeFor(value,root)!=NULL;//IS THIS ALLOWED?
+    return getNodeFor(value,root)!= NULL;//IS THIS ALLOWED?
 
      // REPLACE THIS NON-SOLUTION
 }
@@ -659,17 +331,20 @@ bool Cards::remove(int value){
 
 }
 
+//left side has larger values
 int Cards::minValue() const{
-    Node* val=root;
-    while(val->left){
-        val=val->left;
+    Node *min = root;
+    while(min->left){
+        min = min->left;
     }
-    return val->info;
+    return min->info;
 }
+
+//right side has larger values
 int Cards::maxValue() const {
-    Node* val=root;
-    while(val->right){
-        val=val->right;
+    Node *max = root;
+    while(max->right){
+        max = max->right;
     }
-    return val->info;
+    return max->info;
 }
--- main.cpp
+++ main.cpp
@@ -1,261 +1,5 @@
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-
-// #include "cards.h"
-
-// using namespace std;
-
-// int CardToInt(string card){
-//     int val;
-//     string type = card.substr(2,2);
-//     // char num = card[2];
-//     // char suite = card[0];
-//     // card.substr(2,2);
-
-//     // edge cases for ace, jackm, queenm, king
-//     // switch (num) {
-//     //     case 'a':
-//     //         val = 1;
-//     //         break;
-//     //     case 'j':
-//     //         val = 11;
-//     //         break;
-//     //     case 'q':
-//     //         val = 12;
-//     //         break;
-//     //     case 'k':
-//     //         val = 13;
-//     //         break;
-//     //     default:
-//     //         val = stoi(card.substr(2,2));
-//     //         break;
-//     // }
-
-//     if (type == "a") {
-//         val = 1;
-//     }
-//     else if (type == "j") {
-//         val = 11;
-//     }
-//     else if (type == "q") {
-//         val = 12;
-//     }
-//     else if (type == "k") {
-//         val = 13;
-//     }
-//     else {
-//         val = stoi(type);
-//     }
-
-
-//     // edge cases for diamonds, spades, and hearts
-//     // switch (suite) {
-//     //     case 'd':
-//     //         val += 13;
-//     //         break;
-//     //     case 's':
-//     //         val += 26;
-//     //         break;
-//     //     case 'h':
-//     //         val += 39;
-//     //         break;
-//     //     default:
-//     //         val += 0;
-//     //         break;
-//     // }
-
-
-//     if (card.substr(0,1) == "d") {
-//         val += 13;
-//     }
-//     else if (card.substr(0,1) == "s") {
-//         val += 26;
-//     }
-//     else if (card.substr(0,1) == "h") {
-//         val += 39;
-//     }
-//     return val;
-// }
-
-// string IntToCard(int val){
-//     string card = "";
-//     string value;
-
-//     if (val < 14) {
-//         if (val == 1) {
-//             value = "a";
-//         }
-//         else if (val == 11) {
-//             value = "j";
-//         }
-//         else if (val == 12) {
-//             value = "q";
-//         }
-//         else if (val == 13) {
-//             value = "k";
-//         }
-//         else {
-//             value = to_string(val);
-//         }
-//         card = card + "c " + value;
-//     }
-//     else if (val < 27) {
-//         if (val - 13 == 1) {
-//             value="a";
-//         }
-//         else if (val - 13 == 11) {
-//             value = "j";
-//         }
-//         else if (val - 13 == 12) {
-//             value = "q";
-//         }
-//         else if (val - 13 == 13) {
-//             value = "k";
-//         }
-//         else {
-//             value = to_string(val - 13);
-//         }
-//         card = card + "d " + value;
-//     }
-//     else if (val < 40) {
-//         if (val - 26 == 1) {
-//             value = "a";
-//         }
-//         else if (val - 26 == 11) {
-//             value = "j";
-//         }
-//         else if (val - 26 == 12) {
-//             value = "q";
-//         }
-//         else if (val - 26 == 13) {
-//             value = "k";
-//         }
-//         else {
-//             value = to_string(val - 26);
-//         }
-//         card = card + "s " + value;
-//     }
-//     else {
-//       if (val - 39 == 1) {
-//           value = "a";
-//       }
-//       else if (val - 39 == 11) {
-//           value = "j";
-//       }
-//       else if (val - 39 == 12) {
-//           value = "q";
-//       }
-//       else if (val - 39 == 13) {
-//           value = "k";
-//       }
-//       else {
-//           value = to_string(val - 39);
-//       }
-//       card = card + "h " + value;
-//     }
-//     return card;
-// }
-
-
-// int main(int argv, char** argc){
-  
-//   //prompt lines
-//   if(argv < 3){
-//     cout << "Please provide 2 file names" << endl;
-//     return 1;
-//   }
-  
-//   ifstream cardFile1 (argc[1]);
-//   ifstream cardFile2 (argc[2]);
-//   string line;
-
-//   //check to make sure file can be read
-//   if (cardFile1.fail() || cardFile2.fail() ){
-//     cout << "Could not open file " << argc[2];
-//     return 1;
-//   }
-
-//   //Read each file
-//   Cards aHand;
-//   while (getline (cardFile1, line) && (line.length() > 0)){
-//     aHand.insert(CardToInt(line));
-//   }
-//   cardFile1.close();
-
-//   Cards bHand;
-//   while (getline (cardFile2, line) && (line.length() > 0)){  
-//     bHand.insert(CardToInt(line));
-//   }
-//   cardFile2.close();
-  
-//   //game
-//   int count=0;
-//   int val1min=aHand.minValue();
-//   int val2max=bHand.maxValue();
-//   int valDel;
-//   while(val1min!=aHand.maxValue() && val2max!=bHand.minValue()){  
-//     //Alice
-//     if(count==0){
-//       if(bHand.contains(val1min)){
-//         cout << "Alice picked matching card " << IntToCard(val1min) << endl;
-//         valDel=val1min;
-//         val1min=aHand.getSuccessor(val1min);
-//         aHand.remove(valDel);
-//         /*if(val2max==val1min){
-//           val2max=bHand.getPredecessor(val2max);
-//         }*/
-//         bHand.remove(valDel);
-//         count++;
-//       }
-//       else{
-//         if(!aHand.contains(val1min)){ break;}
-//         val1min=aHand.getSuccessor(val1min);
-
-//       }
-      
-//     }
-//     //Bob
-//     else{
-//       if(aHand.contains(val2max)){
-//         cout << "Bob picked matching card " << IntToCard(val2max) << endl;
-//         valDel=val2max;
-//         val2max=bHand.getPredecessor(val2max);
-//         bHand.remove(valDel);
-//         /*if(val1min==val2max){
-//           val1min=aHand.getSuccessor(val1min);
-//         }*/
-        
-//         aHand.remove(valDel);
-//         count--;
-//       }
-//       else{
-//         if(!bHand.contains(val2max)){ break;}
-//         val2max=bHand.getPredecessor(val2max);
-//       }
-      
-//     }
-//   }
-
-//   cout << endl;
-//   int printVal=aHand.minValue();
-//   cout << "Alice's cards:" << endl;
-//   for(int i=0;i<aHand.count();i++){
-//     cout << IntToCard(printVal) << endl;
-//     printVal=aHand.getSuccessor(printVal);
-//   }
-
-//   cout << endl;
-//   printVal=bHand.minValue();
-//   cout << "Bob's cards:" << endl;
-//   for(int i=0;i<bHand.count();i++){
-//     cout << IntToCard(printVal) << endl;
-//     printVal=bHand.getSuccessor(printVal);
-//   }
-  
-
-//   return 0;
-// }
+// main.cpp
+// NAME: ESHAN JOSHI
 
 #include <iostream>
 #include <fstream>
@@ -266,74 +10,160 @@
 using namespace std;
 
 int CardToInt(string card){
-  int value;
-  if(card.substr(2,2)=="a"){
-    value=1;
-  }
-  else if(card.substr(2,2)=="j"){
-    value=11;
-  }
-  else if(card.substr(2,2)=="q"){
-    value=12;
-  }
-  else if(card.substr(2,2)=="k"){
-    value=13;
-  }
-  else{
-    value=stoi(card.substr(2,2));
-  }
-  if(card.substr(0,1)=="d"){
-    value+=13;
-  }
-  else if(card.substr(0,1)=="s"){
-    value+=26;
-  }
-  else if(card.substr(0,1)=="h"){
-    value+=39;
-  }
-  return value;
+    int val;
+    string type = card.substr(2,2);
+    // char num = card[2];
+    // char suite = card[0];
+    // card.substr(2,2);
+
+    // edge cases for ace, jackm, queenm, king
+    // switch (num) {
+    //     case 'a':
+    //         val = 1;
+    //         break;
+    //     case 'j':
+    //         val = 11;
+    //         break;
+    //     case 'q':
+    //         val = 12;
+    //         break;
+    //     case 'k':
+    //         val = 13;
+    //         break;
+    //     default:
+    //         val = stoi(card.substr(2,2));
+    //         break;
+    // }
+
+    if (type == "a") {
+        val = 1;
+    }
+    else if (type == "j") {
+        val = 11;
+    }
+    else if (type == "q") {
+        val = 12;
+    }
+    else if (type == "k") {
+        val = 13;
+    }
+    else {
+        val = stoi(type);
+    }
+
+
+    // edge cases for diamonds, spades, and hearts
+    // switch (suite) {
+    //     case 'd':
+    //         val += 13;
+    //         break;
+    //     case 's':
+    //         val += 26;
+    //         break;
+    //     case 'h':
+    //         val += 39;
+    //         break;
+    //     default:
+    //         val += 0;
+    //         break;
+    // }
+
+
+    if (card.substr(0,1) == "d") {
+        val += 13;
+    }
+    else if (card.substr(0,1) == "s") {
+        val += 26;
+    }
+    else if (card.substr(0,1) == "h") {
+        val += 39;
+    }
+    return val;
 }
 
 string IntToCard(int val){
-  string card="";
-  string value;
-  if(val<14){
-    if(val==1) value="a";
-    else if(val==11){ value="j"; }
-    else if(val==12){ value="q"; }
-    else if(val==13){ value="k"; }
-    else{ value=to_string(val); }
+    string card = "";
+    string value;
 
-    card=card+"c "+value;
-  }
-  else if(val<27){
-    if(val-13==1) value="a";
-    else if(val-13==11){ value="j"; }
-    else if(val-13==12){ value="q"; }
-    else if(val-13==13){ value="k"; }
-    else{ value=to_string(val-13); }
-    card=card+"d "+value;
-  }
-  else if(val<40){
-    if(val-26==1) value="a";
-    else if(val-26==11){ value="j"; }
-    else if(val-26==12){ value="q"; }
-    else if(val-26==13){ value="k"; }
-    else{ value=to_string(val-26); }
-    card=card+"s "+value;
-  }
-  else{
-    if(val-39==1) value="a";
-    else if(val-39==11){ value="j"; }
-    else if(val-39==12){ value="q"; }
-    else if(val-39==13){ value="k"; }
-    else{ value=to_string(val-39); }
-    card=card+"h "+value;
-  }
-  return card;
+    if (val < 14) {
+        if (val == 1) {
+            value = "a";
+        }
+        else if (val == 11) {
+            value = "j";
+        }
+        else if (val == 12) {
+            value = "q";
+        }
+        else if (val == 13) {
+            value = "k";
+        }
+        else {
+            value = to_string(val);
+        }
+        card = card + "c " + value;
+    }
+    else if (val < 27) {
+        if (val - 13 == 1) {
+            value="a";
+        }
+        else if (val - 13 == 11) {
+            value = "j";
+        }
+        else if (val - 13 == 12) {
+            value = "q";
+        }
+        else if (val - 13 == 13) {
+            value = "k";
+        }
+        else {
+            value = to_string(val - 13);
+        }
+        card = card + "d " + value;
+    }
+    else if (val < 40) {
+        if (val - 26 == 1) {
+            value = "a";
+        }
+        else if (val - 26 == 11) {
+            value = "j";
+        }
+        else if (val - 26 == 12) {
+            value = "q";
+        }
+        else if (val - 26 == 13) {
+            value = "k";
+        }
+        else {
+            value = to_string(val - 26);
+        }
+        card = card + "s " + value;
+    }
+    else {
+      if (val - 39 == 1) {
+          value = "a";
+      }
+      else if (val - 39 == 11) {
+          value = "j";
+      }
+      else if (val - 39 == 12) {
+          value = "q";
+      }
+      else if (val - 39 == 13) {
+          value = "k";
+      }
+      else {
+          value = to_string(val - 39);
+      }
+      card = card + "h " + value;
+    }
+    return card;
 }
 
+
 int main(int argv, char** argc){
+  
+  //prompt lines
   if(argv < 3){
     cout << "Please provide 2 file names" << endl;
     return 1;
@@ -343,6 +173,7 @@ int main(int argv, char** argc){
   ifstream cardFile2 (argc[2]);
   string line;
 
+  //check to make sure file can be read
   if (cardFile1.fail() || cardFile2.fail() ){
     cout << "Could not open file " << argc[2];
     return 1;
